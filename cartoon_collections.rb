bc245be6e055fa37bd0ebb08c6b12e13c51a6aa5
Daniel Vo <daniiielvo@gmail.com>
2020-04-23 18:52:29 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -9,7 +9,7 @@ def summon_captain_planet(planeteer_calls)
     calls.capitalize << "!"
 end
 end
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls
   # Your code here
 end
 

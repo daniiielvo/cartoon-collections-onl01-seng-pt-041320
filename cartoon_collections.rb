7f875b3c31d6645fc8afea4996ad814909e99ea4
Daniel Vo <daniiielvo@gmail.com>
2020-04-23 18:47:18 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,9 +1,11 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarves)
+  dwarves.each_with_index do |names, index|
+ index += 1 
+ puts "#{index} #{names}"
 end
-
-def summon_captain_planet# code an argument here
-  # Your code here
+end
+def summon_captain_planet(planeteer_calls)
+  
 end
 
 def long_planeteer_calls# code an argument here
